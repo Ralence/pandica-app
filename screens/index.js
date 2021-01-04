05b6d3b37fd59c0d39727bc0877e2d207e3128d8
Ralence <ranko.ostojic@gmail.com>
2021-01-04 16:09:49 +0100

    feat: add custom bottom tabs

---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Order from "./Order";
+import OrderItem from "./OrderItem";
+import Profile from "./Profile";
+import Favorite from "./Favorite";
+
+export { Home, Order, OrderItem, Favorite, Profile };
